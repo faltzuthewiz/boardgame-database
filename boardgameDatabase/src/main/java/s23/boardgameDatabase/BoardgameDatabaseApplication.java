03661ae1ff4bdfaf50b706424dc4ba303a66fa4c
Sonja Fallstrom <jawnlemon@gmail.com>
2023-10-05 13:44:37 +0300

    First commit

---
+++ boardgameDatabase/src/main/java/s23/boardgameDatabase/BoardgameDatabaseApplication.java
@@ -0,0 +1,13 @@
+package s23.boardgameDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameDatabaseApplication.class, args);
+	}
+
+}
---
+++ boardgameDatabase/src/test/java/s23/boardgameDatabase/BoardgameDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package s23.boardgameDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
