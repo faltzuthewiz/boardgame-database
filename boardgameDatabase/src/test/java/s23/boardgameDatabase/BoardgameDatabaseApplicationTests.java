0fa0343fa95cc95b1294dd3610b2b85e401c8097
Sonja Fallstrom <jawnlemon@gmail.com>
2023-10-12 13:32:00 +0300

    added some tests

--- boardgameDatabase/src/test/java/s23/boardgameDatabase/BoardgameDatabaseApplicationTests.java
+++ boardgameDatabase/src/test/java/s23/boardgameDatabase/BoardgameDatabaseApplicationTests.java
@@ -1,13 +1,21 @@
 package s23.boardgameDatabase;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import s23.boardgameDatabase.web.BoardGameController;
 
 @SpringBootTest
 class BoardgameDatabaseApplicationTests {
+	
+	@Autowired
+	private BoardGameController controller;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
